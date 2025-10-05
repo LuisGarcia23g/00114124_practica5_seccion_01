ce5c40156312fa7cc78097910c9ec149fb66e202
LuisGarcia23g <00114124@uca.edu.sv>
2025-10-04 22:32:23 -0600

    🍻: subiendo archivos

---
+++ Ejercicio-Final/Data/Scientists.js
@@ -0,0 +1,19 @@
+import Galileo from '../src/assets/Galileo.jpg'
+import Louis from '../src/assets/Louis.jpg'
+
+export const Scientists = [
+{
+name: "Galileo Galilei",
+image: Galileo,
+profession: "Astrónomo, Físico e Ingeniero",
+awards: "Ninguno",
+discoveries: "Júpiter tiene lunas, las fases de Venus, la ley del péndulo, mejoras al telescopio"
+},
+{
+name: "Louis Pasteur",
+image: Louis,
+profession: "Científico y Microbiólogo",
+awards: "Medalla Copley, Medalla Rumford",
+discoveries: "Principios de la vacunación, la pasteurización, la teoría germinal de las enfermedades"
+}
+];
